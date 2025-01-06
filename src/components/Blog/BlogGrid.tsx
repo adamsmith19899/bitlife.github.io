32f2c4dfd2a24ff99904002d9f16521655ef60bb
Waqas <130844309+adamsmith19899@users.noreply.github.com>
2025-01-07 01:37:29 +0500

    Update BlogGrid.tsx

--- src/components/Blog/BlogGrid.tsx
+++ src/components/Blog/BlogGrid.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BlogCard } from './BlogCard';
 import { BlogPost } from '../../types/blog';
 
@@ -10,7 +11,9 @@ export const BlogGrid: React.FC<BlogGridProps> = ({ posts }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {posts.map((post) => (
-        <BlogCard key={post.id} post={post} />
+        <Link key={post.id} to={`/blog/${post.id}`}>
+          <BlogCard post={post} />
+        </Link>
       ))}
     </div>
   );
