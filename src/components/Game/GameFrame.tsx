e0236d39533b1d5ff89b2a1f5f26bf4ae8e36cac
Waqas <130844309+adamsmith19899@users.noreply.github.com>
2025-02-07 23:58:03 +0500

    Update GameFrame.tsx

--- src/components/Game/GameFrame.tsx
+++ src/components/Game/GameFrame.tsx
@@ -17,7 +17,7 @@ export const GameFrame: React.FC<GameFrameProps> = ({ url }) => {
       className={`relative ${
         isFullscreen 
           ? 'fixed inset-0 z-50' 
-          : 'w-600px aspect-video max-w-5xl'
+          : 'w-auto aspect-video max-w-5xl'
       }`}
     >
       <div className="absolute top-2 md:top-4 right-2 md:right-4 z-10">
